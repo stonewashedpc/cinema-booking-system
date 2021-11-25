2feeeefa792b4460021db32f3f9e424755e199d7
unknown <54364500+stonewashedpc@users.noreply.github.com>
2021-11-25 21:25:53 +0100

    Added ClientException and ServerException

---
+++ src/exceptions/ClientException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+import java.io.Serializable;
+
+public class ClientException extends Exception implements Serializable {
+	private static final long serialVersionUID = 1L;
+	public ClientException(String message) {
+		super(message);
+	}
+}
