77008382b1e2ac3188cdf81ccc1ff5d4be093285
stonewashedpc <stonewashedpc@gmail.com>
2021-11-29 01:19:49 +0100

    Update ListeningState Class
    The ListeningState Class now verifies the Authentication Token sent with each Command before its execution.

--- src/server/ListeningState.java
+++ src/server/ListeningState.java
@@ -1,7 +1,10 @@
 package server;
 
+import authentication.AuthenticationService;
+import authentication.DefaultWhitelist;
 import commands.Command;
 import exceptions.ServerException;
+import generated.cinemaService.AuthenticationException;
 
 public class ListeningState extends ServerState {
 
@@ -16,8 +19,21 @@ public class ListeningState extends ServerState {
 			Command<?> next;
 			while ((next = (Command<?>) this.getMyServerThread().getIn().readObject()) != null) {
 				System.out.println("Processing new command for client: " + this.getMyServerThread().getClientHostname());
-				next.execute();
-				this.getMyServerThread().getOut().writeObject(next);
+				
+				try {
+					if (next.getAuthToken() == null) { // No AuthToken
+						if(DefaultWhitelist.getInstance().isWhitelisted(next.getClass())) { // Default Permissions sufficient
+							next.execute();
+						} else throw new AuthenticationException("Insufficient permissions to execute command"); // Insufficient Default Permissions
+					} else if (AuthenticationService.getInstance().findRole(next.getAuthToken()).getCommandWhitelist().isWhitelisted(next.getClass())) { // Role Permissions sufficient
+						next.execute();
+					} else throw new AuthenticationException("Insufficient permissions to execute command"); // Insufficient Role Permissions
+				} catch (AuthenticationException e) {
+					next.setE(e); // Invalid AuthToken
+				} finally {
+					this.getMyServerThread().getOut().writeObject(next);
+				}
+				
 			}
 		} catch (Exception e) {
 			throw new ServerException("An error occurred while handling commands for client: "
