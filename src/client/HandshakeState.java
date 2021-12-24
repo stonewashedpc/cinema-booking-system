d46b2a11aa9c198d51be35425dd79f6ede6509d5
stonewashedpc <stonewashedpc@gmail.com>
2021-12-24 01:31:23 +0100

    Add comments and logging to HandshakeStates

--- src/client/HandshakeState.java
+++ src/client/HandshakeState.java
@@ -28,25 +28,30 @@ public class HandshakeState extends ClientState {
 			this.getMyClient().setClientSocket(new Socket(this.getMyClient().getIp(), this.getMyClient().getPort()));
 			this.getMyClient().getClientSocket().setSoTimeout(1000); // Timout read from inputstream
 			
+			// Generate AES-SecretKey
 			SecretKey aesKey = Cryptography.generateSecretKey();
 			
-			// Receiving the RSA - PublicKey
+			System.out.println("Generated AES-SecretKey");
+			
+			// START ============= Receive RSA-PublicKey from Client
 	        ObjectInputStream objectInputStream = new ObjectInputStream(this.getMyClient().getClientSocket().getInputStream());
-	        System.out.println("Client > INFO > created objectInputStream: " + objectInputStream.toString());
 
-	        PublicKey publicKey1 = (PublicKey) objectInputStream.readObject();
-	        System.out.println("Client > INFO > publicKey received from server: " + publicKey1.toString());
-
-	        // Creating a Cipher object
+	        PublicKey publicKey = (PublicKey) objectInputStream.readObject();
+	        System.out.println("Received RSA-PublicKey from server: " + publicKey.toString());
+	        // END =============== Receive RSA-PublicKey from Client
+	        
+	        // START ============= Initialize Cipher for RSA encryption
 	        Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
-	        // Initializing the Cipher object for encrypting
-	        rsaCipher.init(Cipher.ENCRYPT_MODE, publicKey1);
-
+	        rsaCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+	        // END =============== Initialize Cipher for RSA encryption
+	        
+	        // Encrypt AES-SecretKey with RSA-Cipher
 	        byte[] encryptedKey = rsaCipher.doFinal(aesKey.getEncoded());
-	        //System.out.println("Client > INFO > sending encrypted aesKey: " + Arrays.toString(encryptedKey));
-
-	        // Sending the AES - SecretKey encrypted with the RSA - PublicKey
+	        
+	        System.out.println("Encrypted AES-SecretKey using RSA-PublicKey");
+	        
+	        // START ============= Send RSA-encrypted AES-Key to server
 	        DataOutputStream dataOutputStream = new DataOutputStream(this.getMyClient().getClientSocket().getOutputStream());
 
 	        dataOutputStream.writeInt(encryptedKey.length);
@@ -54,25 +59,34 @@ public class HandshakeState extends ClientState {
 
 	        dataOutputStream.write(encryptedKey);
 	        dataOutputStream.flush();
+	        
+	        System.out.println("Sent encrypted AES-Key to server");
+	        // END =============== Send RSA-encrypted AES-Key to server
 
-	        // Creating an ObjectOutputStream over a CipherOutputStream
-	        //Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+	        // Initialize Ciphers (Important: use CTR-Mode)
 	        Cipher aesCipherEncrypt = Cipher.getInstance("AES/CTR/NoPadding");
 	        aesCipherEncrypt.init(Cipher.ENCRYPT_MODE, aesKey, new IvParameterSpec(new byte[16]));
 	        
 	        Cipher aesCipherDecrypt = Cipher.getInstance("AES/CTR/NoPadding");
 			aesCipherDecrypt.init(Cipher.DECRYPT_MODE, aesKey, new IvParameterSpec(new byte[16]));
 	        
+			// Initialize OutputStream
 	        CipherOutputStream cipherOutputStream = new CipherOutputStream(this.getMyClient().getClientSocket().getOutputStream(), aesCipherEncrypt);
 	        
 	        this.getMyClient().setOut(new ObjectOutputStream(cipherOutputStream));
 	        
 	        this.getMyClient().getOut().flush();
-
+	        
+	        System.out.println("ObjectOutputStream initialized");
+	        
+	        // Initialize InputStream
 	        CipherInputStream cipherInputStream = new CipherInputStream(this.getMyClient().getClientSocket().getInputStream(), aesCipherDecrypt);
 	        
 	        this.getMyClient().setIn(new ObjectInputStream(cipherInputStream));
 	        
+	        System.out.println("ObjectInputStream initialized");
+	        
+	        // Set ClientState to ReadyState (Ready to send commands to server)
 			this.getMyClient().setClientState(new ReadyState(this.getMyClient()));
 			System.out.println("Client ready");
 		} catch (Exception e) {
--- src/server/HandshakeState.java
+++ src/server/HandshakeState.java
@@ -27,63 +27,71 @@ public class HandshakeState extends ServerState {
 		try {
 			this.getMyServerThread().getClientSocket().setSoTimeout(600000); // Timeout after 10 minutes (600000 milliseconds)
 			
+			// START ============= Generate RSA-KeyPair and send to Client
 			KeyPair keyPair = Cryptography.generateKeyPair();
 			
+			System.out.println("Generated RSA-KeyPair");
+			
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(this.getMyServerThread().getClientSocket().getOutputStream());
-			System.out.println("Server > INFO > created objectOutputStream: " + objectOutputStream.toString());
 
 			objectOutputStream.writeObject(keyPair.getPublic());
 			objectOutputStream.flush();
+			
+			System.out.println("Sent RSA-PublicKey to Client");
+			// END =============== Generate RSA-KeyPair and send to Client
 
-			// Receiving the encrypted AES - SecretKey
+			// START ============= Receive encrypted AES-Key from Client
 			DataInputStream dataInputStream = new DataInputStream(this.getMyServerThread().getClientSocket().getInputStream());
 
 			int length = dataInputStream.readInt();
-			System.out.println("Server > INFO > received encrypted aesKey's length: " + length);
+			System.out.println("Received encrypted AES-Key length: " + length + " bytes");
 			byte[] encryptedKey = null;
 			if (length > 0) {
 				encryptedKey = new byte[length];
 				dataInputStream.readFully(encryptedKey, 0, encryptedKey.length);
 			} else {
-				System.out.println("Server > ERROR > length received is <= 0");
+				System.out.println("ERROR: length received for encrypted AES-Key is <= 0");
 				this.endOfProcessing();
 			}
+			// END =============== Receive encrypted AES-Key from Client
 
-			System.out.println("Server > INFO > received encrypted aesKey: " + Arrays.toString(encryptedKey));
-
-			// Decrypting the encrypted AES - SecretKey
+			System.out.println("Received encrypted AES-Key: " + Arrays.toString(encryptedKey));
+			
+			// Decrypt encrypted AES-Key
 			Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
 			rsaCipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
 			byte[] decryptedKey = rsaCipher.doFinal(encryptedKey);
 
-			// Converting the decrypted AES - SecretKey to a SecretKey
+			// Create SecretKey from decrypted AES-Key
 			SecretKey aesKey = new SecretKeySpec(decryptedKey, 0, decryptedKey.length, "AES");
 
-			System.out.println("Server > INFO > converted decrypted aesKey: " + Arrays.toString(aesKey.getEncoded()));
+			System.out.println("Created AES-SecretKey from decrypted AES-Key: " + Arrays.toString(aesKey.getEncoded()));
 			
-			//Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			// Initialize Ciphers (Important: use CTR-Mode)
 			Cipher aesCipherDecrypt = Cipher.getInstance("AES/CTR/NoPadding");
 			aesCipherDecrypt.init(Cipher.DECRYPT_MODE, aesKey, new IvParameterSpec(new byte[16]));
 			
 			Cipher aesCipherEncrypt = Cipher.getInstance("AES/CTR/NoPadding");
 	        aesCipherEncrypt.init(Cipher.ENCRYPT_MODE, aesKey, new IvParameterSpec(new byte[16]));
 			
+	        // Initialize OutputStream
 			CipherOutputStream cipherOutputStream = new CipherOutputStream(this.getMyServerThread().getClientSocket().getOutputStream(), aesCipherEncrypt);
-			System.out.println("Server > INFO > created cipherOutputStream");
 			
 			this.getMyServerThread().setOut(new ObjectOutputStream(cipherOutputStream));
-			System.out.println("Server > INFO > created ObjectOutputStream");
 			
 			this.getMyServerThread().getOut().flush();
+			
+			System.out.println("ObjectOutputStream initialized");
 
-			// Creating an ObjectInputStream over a CipherInputStream
+			// Initialize InputStream
 			CipherInputStream cipherInputStream = new CipherInputStream(this.getMyServerThread().getClientSocket().getInputStream(), aesCipherDecrypt);
-			System.out.println("Server > INFO > created cipherInputStream");
 			
 			this.getMyServerThread().setIn(new ObjectInputStream(cipherInputStream));
-			System.out.println("Server > INFO > created ObjectInputStream");
 			
+			System.out.println("ObjectInputStream initialized");
+			
+			// Set ServerState to ListeningState (Ready to handle incoming commands)
 			this.getMyServerThread().setServerState(new ListeningState(this.getMyServerThread()));
 			System.out.println("Handshake successful for client: " + this.getMyServerThread().getClientHostname());
 		} catch (Exception e) {
