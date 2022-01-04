bc3b50b17a462037df301134a76e01e70f3862e2
stonewashedpc <stonewashedpc@gmail.com>
2022-01-04 21:18:27 +0100

    Update CustomerWhiteList.java

--- src/authentication/CustomerWhiteList.java
+++ src/authentication/CustomerWhiteList.java
@@ -5,11 +5,17 @@ import java.util.HashSet;
 
 import generated.cinemaService.commands.getCShowCache_Command;
 import generated.cinemaService.commands.getReservableShows_Command;
+import generated.cinemaService.commands.getReservations_Command;
 import generated.cinemaService.commands.logout_Command;
 import generated.cinemaService.commands.reserve_Command;
 import generated.cinemaService.commands.cShow.CShow_getFilm_Command;
 import generated.cinemaService.commands.cShow.CShow_getHall_Command;
 import generated.cinemaService.commands.cShow.CShow_getReservableSeats_Command;
+import generated.cinemaService.commands.reservation.Reservation_getBooking_Command;
+import generated.cinemaService.commands.reservation.Reservation_getSeat_Command;
+import generated.cinemaService.commands.reservation.Reservation_getShow_Command;
+import generated.cinemaService.commands.reservation.Reservation_isBooked_Command;
+import generated.cinemaService.commands.seat.Seat_getPrice_Command;
 import generated.cinemaService.commands.seat.Seat_getRow_Command;
 import generated.cinemaService.commands.seatingRow.SeatingRow_getCategoryName_Command;
 import generated.cinemaService.commands.seatingRow.SeatingRow_getCategory_Command;
@@ -24,8 +30,10 @@ public class CustomerWhiteList extends CommandWhitelist {
 		this.setWhiteList(new HashSet<Class<?>>(Arrays.asList(logout_Command.class, reserve_Command.class,
 				getReservableShows_Command.class, CShow_getFilm_Command.class, CShow_getHall_Command.class,
 				CShow_getReservableSeats_Command.class, SeatingRow_getCategory_Command.class, Seat_getRow_Command.class,
-				SeatingRow_getPrice_Command.class, SeatingRow_getCategoryName_Command.class))); // Define Customer
-																								// Commands
+				SeatingRow_getPrice_Command.class, SeatingRow_getCategoryName_Command.class,
+				getReservations_Command.class, Reservation_getShow_Command.class, Reservation_getSeat_Command.class,
+				Reservation_getBooking_Command.class, Seat_getPrice_Command.class, Reservation_isBooked_Command.class))); // Define Customer
+		// Commands
 		// Here
 		this.includeWhiteList(DefaultWhitelist.getInstance());
 	}
