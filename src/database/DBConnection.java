d508eaf962ddfb9b0e1fc2bba7facb1abb3a97b4
maidasii <m.goschka@gmx.de>
2022-01-05 20:21:16 +0100

    db Configuration corrected

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,18 +1,32 @@
 package database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Properties;
 
 public class DBConnection {
-	final String filename = "jdbc:mysql://localhost:3306/cinemaservice?user=root";
-	
-	public void BuildConnection(String sqlCheck) throws SQLException {
-		try (final Connection conn = DriverManager.getConnection(filename)) {
-			try (final Statement stmt = conn.createStatement()) {
-				stmt.executeUpdate(sqlCheck);
-			}
+
+	public Connection creatConnection() throws SQLException {
+
+		Connection conn = null;
+
+		try {
+
+			Properties props = new Properties();
+			props.load(new FileInputStream("./src/config/DBConfiguration.properties"));
+			String user = props.getProperty("user");
+			String password = props.getProperty("password");
+			String dbUrl = props.getProperty("dbUrl");
+
+			final Connection connection = DriverManager.getConnection(dbUrl, user, password);
+			conn = connection;
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		return conn;
 	}
 }
--- src/database/ResetDB.java
+++ src/database/ResetDB.java
@@ -1,15 +1,20 @@
 package database;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ResetDB {
 
-	final String sqlCheck = "TRUNCATE TABLE user_has_password";
-	private DBConnection conn;
-	
-	public void resetDB() throws SQLException {  
-		conn = new DBConnection();
-		conn.BuildConnection(sqlCheck);
+	final String dbResetQuerryForTest = "TRUNCATE TABLE user_has_password";
+	private DBConnection dbConnection;
+	private Connection conn;
+
+	public void resestDBForTest() throws SQLException {
+		dbConnection = new DBConnection();
+		conn = dbConnection.creatConnection();
+		final Statement stmt = conn.createStatement();
+		stmt.executeUpdate(dbResetQuerryForTest);
 	}
 
 }
