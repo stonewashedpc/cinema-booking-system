db5fa39cd2574ab359027e0f5a6b4e54b7ed21b8
Nils Grohnert <nils.grohnert@edu.fhdw.de>
2022-01-05 01:31:09 +0100

    Add Tests for User Login and Logout

--- src/database/ResetDB.java
+++ src/database/ResetDB.java
@@ -7,7 +7,7 @@ public class ResetDB {
 	final String sqlCheck = "TRUNCATE TABLE user_has_password";
 	private DBConnection conn;
 	
-	public void updateDB() throws SQLException {  
+	public void resetDB() throws SQLException {  
 		conn = new DBConnection();
 		conn.BuildConnection(sqlCheck);
 	}
--- src/test/TestServerMain.java
+++ src/test/TestServerMain.java
@@ -6,17 +6,57 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import database.ResetDB;
+import generated.cinemaService.AuthenticationException;
+import generated.cinemaService.CinemaService;
 
 class TestServerMain {
+	
+	private static String authToken;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		ResetDB updateDB = new ResetDB();
-		updateDB.updateDB();
+		ResetDB resetDB = new ResetDB();
+		resetDB.resetDB();
+		authToken = "";
+		
+		CinemaService.getInstance().register("Erika Musterfrau", "1234512345");
+	}
+	
+	@Test
+	public void userLoginTest() {
+		try {
+			CinemaService.getInstance().login("Erika Musterfrau", "123"); 
+		    fail("Exception not thrown when expected!");
+		} catch (AuthenticationException expectedException) {
+		}
+		try {
+			CinemaService.getInstance().login("Erika", "123"); 
+		    fail("Exception not thrown when expected!");
+		} catch (AuthenticationException expectedException) {
+		}
+		try {
+			CinemaService.getInstance().login("Erika", "1234512345"); 
+		    fail("Exception not thrown when expected!");
+		} catch (AuthenticationException expectedException) {
+		}
+		try {
+			authToken = CinemaService.getInstance().login("Erika Musterfrau", "1234512345"); 
+		} catch (AuthenticationException unexpectedException) {
+			fail("Exception thrown when not expected!");
+		}
 	}
 
 	@Test
-	void test() {
-		assertEquals(1 + 2, 3);
+	public void userLogoutTest() {
+		try {
+			CinemaService.getInstance().logout("WrongAuthToken");
+		    fail("Exception not thrown when expected!");
+		} catch (AuthenticationException expectedException) {
+		}
+		try {
+			CinemaService.getInstance().logout(authToken);
+		} catch (AuthenticationException unexpectedException) {
+			fail("Exception thrown when not expected!");
+		}
 	}
 }
