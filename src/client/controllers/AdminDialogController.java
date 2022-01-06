b08b8edaf58b23cfe94919b15b98d055497f58fa
stonewashedpc <stonewashedpc@gmail.com>
2022-01-06 20:39:38 +0100

    Implement Basic Admin Functionality

--- src/client/controllers/AdminDialogController.java
+++ src/client/controllers/AdminDialogController.java
@@ -1,18 +1,65 @@
 package client.controllers;
 
 import client.Client;
+import client.CommandCallback;
+import client.CommandExecutorService;
 import client.forms.AdminDialogForm;
+import generated.cinemaService.Film;
+import generated.cinemaService.Hall;
+import generated.cinemaService.commands.Film_constructor_Command;
+import generated.cinemaService.commands.Hall_constructor_Command;
 
 public class AdminDialogController extends Controller<Client, AdminDialogForm> {
+	
+	private CommandExecutorService executorService;
 
 	public AdminDialogController(Client model, AdminDialogForm view) {
 		super(model, view);
+		this.executorService = new CommandExecutorService(model);
 	}
 
 	@Override
 	protected void init() {
-		// TODO Auto-generated method stub
-		
+		this.view.getButtonCreateHall().addActionListener((e) -> {
+			String hallName = this.view.getTextFieldHallName().getText();
+			if (hallName == "") {
+				view.getLogLabel().setText("Please enter a name for the new hall first");
+				return;
+			}
+			this.executorService.queueCommand(new Hall_constructor_Command(hallName), new CommandCallback<Hall>() {
+
+				@Override
+				protected void onSuccess(Hall result) {
+					view.getLogLabel().setText(String.format("Created new hall \"%s\" successfully", result.getName()));
+				}
+
+				@Override
+				protected void onException(Exception exception) {
+					view.getLogLabel().setText(exception.getMessage());
+					exception.printStackTrace();
+				}
+			});
+		});
+		this.view.getButtonCreateFilm().addActionListener((e) -> {
+			String filmName = this.view.getTextFieldFilmName().getText();
+			if (filmName == "") {
+				view.getLogLabel().setText("Please enter a name for the new film first");
+				return;
+			}
+			this.executorService.queueCommand(new Film_constructor_Command(filmName), new CommandCallback<Film>() {
+
+				@Override
+				protected void onSuccess(Film result) {
+					view.getLogLabel().setText(String.format("Created new film \"%s\" successfully", result.getName()));
+				}
+
+				@Override
+				protected void onException(Exception exception) {
+					view.getLogLabel().setText(exception.getMessage());
+					exception.printStackTrace();
+				}
+			});
+		});
 	}
 
 }
--- src/client/forms/AdminDialogForm.java
+++ src/client/forms/AdminDialogForm.java
@@ -18,11 +18,36 @@ public class AdminDialogForm extends JDialog {
 		initComponents();
 	}
 
+	public JTextField getTextFieldHallName() {
+		return textFieldHallName;
+	}
+
+	public JTextField getTextFieldFilmName() {
+		return textFieldFilmName;
+	}
+
+	public JButton getButtonCreateHall() {
+		return buttonCreateHall;
+	}
+
+	public JButton getButtonCreateFilm() {
+		return buttonCreateFilm;
+	}
+
+	public JLabel getLogLabel() {
+		return logLabel;
+	}
+
 	private void initComponents() {
 		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
 		dialogPane = new JPanel();
 		contentPanel = new JPanel();
+		textFieldHallName = new JTextField();
+		textFieldFilmName = new JTextField();
+		buttonCreateHall = new JButton();
+		buttonCreateFilm = new JButton();
 		buttonBar = new JPanel();
+		logLabel = new JLabel();
 		okButton = new JButton();
 
 		//======== this ========
@@ -40,15 +65,39 @@ public class AdminDialogForm extends JDialog {
 			//======== contentPanel ========
 			{
 
+				//---- buttonCreateHall ----
+				buttonCreateHall.setText("Create Hall");
+
+				//---- buttonCreateFilm ----
+				buttonCreateFilm.setText("Create Film");
+
 				GroupLayout contentPanelLayout = new GroupLayout(contentPanel);
 				contentPanel.setLayout(contentPanelLayout);
 				contentPanelLayout.setHorizontalGroup(
 					contentPanelLayout.createParallelGroup()
-						.addGap(0, 814, Short.MAX_VALUE)
+						.addGroup(contentPanelLayout.createSequentialGroup()
+							.addGroup(contentPanelLayout.createParallelGroup()
+								.addGroup(contentPanelLayout.createSequentialGroup()
+									.addComponent(textFieldHallName, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+									.addComponent(buttonCreateHall))
+								.addGroup(contentPanelLayout.createSequentialGroup()
+									.addComponent(textFieldFilmName, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+									.addComponent(buttonCreateFilm)))
+							.addGap(0, 510, Short.MAX_VALUE))
 				);
 				contentPanelLayout.setVerticalGroup(
 					contentPanelLayout.createParallelGroup()
-						.addGap(0, 404, Short.MAX_VALUE)
+						.addGroup(contentPanelLayout.createSequentialGroup()
+							.addGroup(contentPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+								.addComponent(textFieldHallName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(buttonCreateHall))
+							.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+							.addGroup(contentPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+								.addComponent(textFieldFilmName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(buttonCreateFilm))
+							.addGap(0, 352, Short.MAX_VALUE))
 				);
 			}
 			dialogPane.add(contentPanel, BorderLayout.CENTER);
@@ -59,6 +108,12 @@ public class AdminDialogForm extends JDialog {
 				buttonBar.setLayout(new GridBagLayout());
 				((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 80};
 				((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0};
+
+				//---- logLabel ----
+				logLabel.setText("Log");
+				buttonBar.add(logLabel, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+					new Insets(0, 0, 0, 5), 0, 0));
 
 				//---- okButton ----
 				okButton.setText("OK");
@@ -77,7 +132,12 @@ public class AdminDialogForm extends JDialog {
 	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
 	private JPanel dialogPane;
 	private JPanel contentPanel;
+	private JTextField textFieldHallName;
+	private JTextField textFieldFilmName;
+	private JButton buttonCreateHall;
+	private JButton buttonCreateFilm;
 	private JPanel buttonBar;
+	private JLabel logLabel;
 	private JButton okButton;
 	// JFormDesigner - End of variables declaration  //GEN-END:variables
 }
