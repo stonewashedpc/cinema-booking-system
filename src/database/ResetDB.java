3171dcf87c6ee430cc35b4bee512fa94c797d012
Nils Grohnert <nils.grohnert@edu.fhdw.de>
2022-01-06 22:06:15 +0100

    Update SQL-Statement for Resetting Database

--- src/database/ResetDB.java
+++ src/database/ResetDB.java
@@ -6,7 +6,29 @@ import java.sql.Statement;
 
 public class ResetDB {
 
-	final String dbResetQuerryForTest = "TRUNCATE TABLE user_has_password";
+	final String dbResetQuerryForTest = "SET FOREIGN_KEY_CHECKS = 0;" 
+									+ "TRUNCATE TABLE user_has_role;"
+									+ "TRUNCATE TABLE user_has_password;"
+									+ "TRUNCATE TABLE user_reservation;"
+									+ "TRUNCATE TABLE show_has_film;"
+									+ "TRUNCATE TABLE show_hall;"
+									+ "TRUNCATE TABLE row_seat;"
+									+ "TRUNCATE TABLE row_has_category;"
+									+ "TRUNCATE TABLE reservation_show;"
+									+ "TRUNCATE TABLE reservation_seat;"
+									+ "TRUNCATE TABLE hall_row;"
+									+ "TRUNCATE TABLE booking_for_reservation;"
+									+ "TRUNCATE TABLE user;"
+									+ "TRUNCATE TABLE seatingrow;"
+									+ "TRUNCATE TABLE seat;"
+									+ "TRUNCATE TABLE reservation;"
+									+ "TRUNCATE TABLE password;"
+									+ "TRUNCATE TABLE hall;"
+									+ "TRUNCATE TABLE film;"
+									+ "TRUNCATE TABLE cshow;"
+									+ "TRUNCATE TABLE booking;"
+									+ "SET FOREIGN_KEY_CHECKS = 1;";
+	
 	private DBConnection dbConnection;
 	private Connection conn;
 
@@ -14,7 +36,7 @@ public class ResetDB {
 		dbConnection = new DBConnection();
 		conn = dbConnection.creatConnection();
 		final Statement stmt = conn.createStatement();
-		stmt.executeUpdate(dbResetQuerryForTest);
+		for(String str: dbResetQuerryForTest.split(";")) stmt.addBatch(str);
+		stmt.executeBatch();
 	}
-
 }
