4994315ad7dddab67891ffac233b255daf057b89
unknown <54364500+stonewashedpc@users.noreply.github.com>
2022-01-08 21:01:04 +0100

    Update OwnerWhiteList.java

--- src/authentication/OwnerWhiteList.java
+++ src/authentication/OwnerWhiteList.java
@@ -3,16 +3,27 @@ package authentication;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import generated.cinemaService.commands.CShow_constructor_Command;
 import generated.cinemaService.commands.Film_constructor_Command;
 import generated.cinemaService.commands.Hall_constructor_Command;
+import generated.cinemaService.commands.getFilms_Command;
+import generated.cinemaService.commands.getHalls_Command;
+import generated.cinemaService.commands.getShows_Command;
+import generated.cinemaService.commands.cShow.CShow_setOpenForReservations_Command;
+import generated.cinemaService.commands.cShow.CShow_setReservable_Command;
 
 public class OwnerWhiteList extends CommandWhitelist {
-	
+
 	private static OwnerWhiteList instance;
 
 	private OwnerWhiteList() {
 		super();
-		this.setWhiteList(new HashSet<Class<?>>(Arrays.asList(Hall_constructor_Command.class, Film_constructor_Command.class))); // Define Owner Commands Here
+		this.setWhiteList(new HashSet<Class<?>>(
+				Arrays.asList(Hall_constructor_Command.class, Film_constructor_Command.class, getHalls_Command.class,
+						getShows_Command.class, getFilms_Command.class, CShow_constructor_Command.class,
+						CShow_setReservable_Command.class, CShow_setOpenForReservations_Command.class))); // Define
+																											// Owner
+		// Commands Here
 		this.includeWhiteList(CustomerWhiteList.getInstance());
 	}
 
